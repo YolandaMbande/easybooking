c03db71b70c4226726a96cf33eb90b8762e164d0
YolandaMbande <yolandambande60@gmail.com>
2024-09-29 18:48:36 +0200

    new whatever

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="//unpkg.com/alpinejs" defer></script>
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!--@vite(['resources/css/app.css', 'resources/js/app.js'])-->
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 // Web routes file
 Route::get('/', [EventController::class, 'showEventsWelcomePage'])->name('welcome');
 
